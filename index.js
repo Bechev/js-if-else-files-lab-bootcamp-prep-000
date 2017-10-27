806ce2c6786779719d8b11eeba4ec0ed66e5da19
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2017-10-27 21:53:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var name = "Joe"
 var height = "74"
 
+if(name=== "Suzanne"){
+  console.log("The name is correct")
+} else {
+  console.log("Expected name to be Suzanne")
+}
 
 // Don't worry about this
 module.exports = { name, height
